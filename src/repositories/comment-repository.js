581e1145157f572c781c145fa09fe5131c5ac561
Asmodeus <arjundeshta20@gmail.com>
2024-05-19 03:08:07 +0530

    made some changes in comment api's

--- src/repositories/comment-repository.js
+++ src/repositories/comment-repository.js
@@ -5,6 +5,15 @@ class UserRepository extends CrudRepository {
     constructor() {
         super(Comment);
     }
+    async destroyComment(id) {
+        const comment = await Comment.findOne({
+            id : id
+        });
+        if(comment) {
+            await comment.destroy();
+        }
+        return comment;
+    }
 }
 
 module.exports = UserRepository;
